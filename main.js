286cd3e3c4143a933c5c8c2c1f129e62ca69d39e
Alex-Bubblemaster <shiatzu@gmail.com>
2015-07-23 15:31:55 +0300

    Working bottle images and id trackable during dragging

--- main.js
+++ main.js
@@ -1,78 +1,82 @@
-//add constants with number/alcohol pairs
-var ALCOHOL_CONSTANTS = {
-    0: 'vodka',
-    1:'dark rum',
-    2: 'gin',
-    3: 'tequila' ,
-    4: 'white rum'
-    },
-    SOFTDRINK_CONSTANTS = {
-        0: 'orange juice',
-        1: 'tomato juice',
-        2: 'sprite',
-        3: 'coke',
-        4: 'cranberry juice'
+function loadImages(sources, callback) {
+    var images = {};
+    var loadedImages = 0;
+    var numImages = 0;
+
+    for (var src in sources) {
+        numImages++;
+    }
+
+    for (var src in sources) {
+        images[src] = new Image();
+        images[src].onload = function () {
+            if (++loadedImages >= numImages) {
+                callback(images);
+            }
+        };
+
+        images[src].src = sources[src];
+    }
+}
+
+function initStage(images) {
+    var stage = new Kinetic.Stage({
+        container: 'container',
+        width: 1500,
+        height: 800
+    });
+
+    var layer = new Kinetic.Layer();
+    var ALCOHOL_CONSTANTS = {
+        0: 'vodka',
+        1: 'gin',
+        2: 'cointreau',
+        3: 'tequila',
+        4: 'white rum'
     };
+    var BOTTLES_OFF_SET_POSITION = 60,
+        bottleID,
+        bottles = [];
 
-var stage = new Kinetic.Stage({
-    container: 'container',
-    width: 800,
-    height: 500
-});
-var layer = new Kinetic.Layer();
-//var bgLayer = new Kinetic.Layer();
-stage.add(layer);
-
-function startAddBottles() {
-    var bottles = [],
-        numberOfAlcoholBottles = 5,
-        bottlesOffsetPosition = 60,
-        bottleID;
-
-    for (bottleID = 0; bottleID < numberOfAlcoholBottles; bottleID += 1) { //alcohol bottles here
-        var bottle = new Kinetic.Rect({
-            x: 10 + bottleID * bottlesOffsetPosition,
+    for (bottleID = 0; bottleID < 3; bottleID += 1) { //alcohol bottles here
+        var imageObj = new Image();
+        var bottle = new Kinetic.Image({
+            x: 10 + bottleID * BOTTLES_OFF_SET_POSITION,
             y: 15,
+            image: images[bottleID],
             width: 50,
             height: 100,
-            fill: 'lightblue',
-            stroke: 'blue'
+            draggable: true,
         });
         bottle.id = ALCOHOL_CONSTANTS[bottleID];
         bottles.push(bottle);
+
         layer.add(bottle);
-   }
-
-    bottles.forEach(function (bottle) {
-        bottle.addEventListener('click', function () {
-             console.log(bottle.id);  // add the clicked alcohol to compare with constants
-        })
-    })
-
-    var softDrinks = [],
-        numberOfSoftDrinks = 5;
-
-    for (bottleID = 0; bottleID < numberOfSoftDrinks; bottleID +=1) {
-        var softDrink = new Kinetic.Rect({
-            x: 10 + bottleID * bottlesOffsetPosition,
-            y: 150,
-            width: 50,
-            height: 100,
-            fill: 'lightblue',
-            stroke: 'blue'
-        });
-        softDrink.id = SOFTDRINK_CONSTANTS[bottleID];
-        softDrinks.push(softDrink);
-        layer.add(softDrink);
+        stage.add(layer);
     }
-
-    softDrinks.forEach(function (softDrink) {
-        softDrink.addEventListener('click', function () {
-            console.log(softDrink.id);  // add the clicked soft drinks to compare with constants
+    bottles.forEach(function (bottle) {
+        bottle.addEventListener('dragstart', function () {
+            console.log(bottle.id);  // add the clicked alcohol to compare with constants
         })
     })
 
-    layer.draw();
+    layer.on('mouseover', function (evt) {
+        var shape = evt.shape;
+        document.body.style.cursor = 'pointer';
+        layer.draw();
+    });
+
+    layer.on('mouseout', function (evt) {
+        var shape = evt.shape;
+        document.body.style.cursor = 'default';
+        layer.draw();
+    });
 }
 
-startAddBottles();
+var sources = {
+    0: 'images/absolutVodka_250x500.jpg',
+    1: 'images/gordon_gin.png',
+    2: 'images/coin.png'
+};
+
+loadImages(sources, initStage);
