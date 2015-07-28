2842578402cd608710e6a4180db9f413a2c2277a
Alex-Bubblemaster <shiatzu@gmail.com>
2015-07-28 14:42:13 +0300

    Added new sounds for Evlogi & Ivo. Updated sound array in main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@
         cocktail = RECIPES[index],
         stage = null,
         layer = null,
-        backgroundLayer = null;
+        backgroundLayer = null,
     bartendersSounds = {
         'niki': [
             'sounds/Niki/Niki - i sys sigurnost shte psuvate.wav',
@@ -29,18 +29,34 @@
             'sounds/Niki/Niki - Vsichko e prekrasno.wav'
         ],
         'doncho': [
-            'sounds/Doncho/Doncho - nazdrave.wav',
-            'sounds/Doncho/Doncho - v kenvasa nazdrave.wav'
+            'sounds/Doncho/Doncho - Blagodarya blagodarya vi.wav',
+            'sounds/Doncho/Doncho - Chakaite da se setya kak tochno beshe formulata.wav',
+            'sounds/Doncho/Doncho - Imame taka dobri predlozheniya.wav',
+            'sounds/Doncho/Doncho - NAZDRAVE.wav',
+            'sounds/Doncho/Doncho - ne e mnogo mydro reshenie.wav',
+            'sounds/Doncho/Doncho - v kenvasa nazdrave.wav',
+            'sounds/Doncho/Doncho- Kvo staa siga - nishtu.wav'
         ],
-        'evlogi': ['sounds/Evlogi/Evlogi - Moga da si pravq kvot si iskam.wav'],
+        'evlogi': [
+            'sounds/Evlogi/Evlogi - Izglezhda grozno.wav',
+            'sounds/Evlogi/Evlogi - Kvot mi hrumne na akyla.wav',
+            'sounds/Evlogi/Evlogi - Moga da si pravq kvot si iskam.wav',
+            'sounds/Evlogi/Evlogi - Moga da si pravya vsyakakvi neshta kvot mi hrumne na akyla.wav',
+            'sounds/Evlogi/Evlogi - Ne se plashete.wav',
+            'sounds/Evlogi/Evlogi - Oshte po-grozno ot predi.wav',
+            'sounds/Evlogi/Evlogi - She vidim kak shse spravim.wav',
+            'sounds/Evlogi/Evlogi - Uha bravo be.wav'
+        ],
         'ivo': [
             'sounds/Ivo/Ivo - Kyde sa aplodismentite.wav',
             'sounds/Ivo/Ivo - napravo mi burka v ushite.wav',
+            'sounds/Ivo/Ivo - Nisho ne staa ot nego.wav',
+            'sounds/Ivo/Ivo - Totalen bylvoch.wav',
+            'sounds/Ivo/Ivo - Tva e edin chichka koito e prekalyaval s alkohola obache e mnogo dobyr v programiraneto.wav',
             'sounds/Ivo/Ivo - Tva e redovna greshka taka che vse taq.wav'
         ]
-    },
+    };
 
-    //TODO get event by target/function etc and trigger bartender appearance animation according to selection
     bartenderSelected.addEventListener('click', function (ev) {
         var startScreen = document.getElementById('start-screen').style.display = 'none',
             container = document.getElementById('container').style.display = 'block',
